db9ccba84b61edf861517bf55ce50dd7d4770688
ExxxxT <aleks9308@gmail.com>
2020-09-15 00:54:38 +0300

    1

---
+++ src/ball/balltest.java
@@ -0,0 +1,11 @@
+package ball;
+
+public class balltest {
+        public static void main(String[] args) {
+            Ball b1 = new Ball(235, 55);
+            System.out.println(b1);
+            b1.move(235, 35);
+            System.out.println(b1);
+        }
+    }
+
